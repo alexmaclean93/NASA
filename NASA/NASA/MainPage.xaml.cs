857850b9ae5a1a8946fb2a3e1ebd52077a517ec3
Alex MacLean <alexmaclean1993@gmail.com>
2021-04-05 21:27:54 -0300

    Added working API retrieval

---
+++ NASA/NASA/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using NASA.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NASA
+{
+    public sealed partial class MainPage : Page
+    {
+        public Root dayModel { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            dayModel = new Root();
+            string dateString = "temp";
+            GetDataFromAPI(dateString);
+        }
+
+        private async void GetDataFromAPI(string dateString)
+        {
+            
+            // Example Date range string 
+            //string url = "https://apodapi.herokuapp.com/api/?start_date=2020-01-01&end_date=2020-12-31";
+
+            // Start with 50 random
+            string url = "https://apodapi.herokuapp.com/api/?count=50";
+
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync(url);
+            var data = JsonConvert.DeserializeObject<List<DayModel>>(response);
+
+            string temp = "Break point";
+        }
+    }
+}
---
+++ NASA/NASA/Models/DayModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NASA.Models
+{
+    public class Root
+    {
+        public List<DayModel> data { get; set; }
+    }
+
+    public class DayModel
+    {
+        public string apod_site { get; set; }
+        public string copyright { get; set; }
+        public string date { get; set; }
+        public string description { get; set; }
+        public string hdurl { get; set; }
+        public string media_type { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
